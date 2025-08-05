b438da17039dc5523e29d2feb3ccc6fb96764120
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-08-05 18:55:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+funtiion manipulateArray(){
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve([1,2,3,4]);
+		},3000);
+	});
+}
+manipulateArray()
+.then((array)=> {
+	const evenNumbers = array.filter(num=> num%2===0);
+	setTimeout(()=>{
+		document.getElementById('output').innerText = evenNumbers.join(',');
+	},1000);
+
+	return evenNumbers.map(num=>num*2);
+})
+.then((multipliedArray)=>{
+	setTimeout(()=>{
+		document.getElementById('output').innerText = multipliedArray.join(',')
+	},2000);
+});
+
+
+
+
+
+
+
+
+
+	
+})
