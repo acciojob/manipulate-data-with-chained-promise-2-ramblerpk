42117bd6ef0a7f513a2bf6b68d2d1e3485b2fc81
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-08-05 19:05:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
-//your JS code here. If required.
-funtiion manipulateArray(){
-	return new Promise((resolve)=>{
-		setTimeout(()=>{
-			resolve([1,2,3,4]);
-		},3000);
-	});
+function manipulateArray() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    });
 }
+
 manipulateArray()
-.then((array)=> {
-	const evenNumbers = array.filter(num=> num%2===0);
-	setTimeout(()=>{
-		document.getElementById('output').innerText = evenNumbers.join(',');
-	},1000);
-
-	return evenNumbers.map(num=>num*2);
-})
-.then((multipliedArray)=>{
-	setTimeout(()=>{
-		document.getElementById('output').innerText = multipliedArray.join(',')
-	},2000);
-});
-
-
-
-
-
-
-
-
-
-	
-})
+    .then((array) => {
+        const evenNumbers = array.filter(num => num % 2 === 0);
+        setTimeout(() => {
+            document.getElementById('output').innerText = evenNumbers.join(',');
+        }, 1000);
+        
+        return evenNumbers.map(num => num * 2);
+    })
+    .then((multipliedArray) => {
+        setTimeout(() => {
+            document.getElementById('output').innerText = multipliedArray.join(',');
+        }, 2000);
+    });
